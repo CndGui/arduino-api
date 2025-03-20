790123b2fd4afc50cd386ec7e39a45a09331a41e
Guilherme <work.guilherme@hotmail.com>
2025-03-20 20:02:57 -0300

    feat: initial commit

---
+++ src/test/java/com/guilhermerodrigues/arduino_api/ArduinoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermerodrigues.arduino_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArduinoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
